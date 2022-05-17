56e1fbd14cbb3dee05c98a5499201db75973f5d6
david <shubhamdev95@github.com>
2022-05-17 19:14:41 +0530

    changes

--- github.js
+++ github.js
@@ -1,30 +1,20 @@
-let getAllData = async() => {
-    let emailData = await getEmailAPIdata();
-    console.log(emailData)
-    if (emailData != -1) {
+let getAllData = () => {
+    let url = location.href;
+    let name = ($('.vcard-fullname')) ? $('.vcard-fullname').text().trim() : "N/A";
+    let username = ($('.vcard-username')) ? $('.vcard-username').text().trim() : "N/A";
+    let country = ($(`[itemprop="homeLocation"] span`)) ? $(`[itemprop="homeLocation"] span`).text().trim() : "N/A";
+    if (name && username) {
         $('body').prepend(`<div>
-            <p>UserName: ${emailData.username}</p>
-            <p>Email: ${emailData.email}</p>
-            <p>Name: ${emailData.name}</p>
-            </div>`);
+                <p>Name: ${name}</p>
+                <p>username: ${username}</p>
+                <p>Country: ${country}</p>
+                <p>URL: ${url}</p>
+                </div>`);
     }
 
-}
 
-let getEmailAPIdata = () => {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            type: 'GET',
-            url: `https://github.com/settings/emails`,
-            success: (data) => {
-                let email = $($.parseHTML(data)).find('#settings-emails h4').text().trim();
-                let username = $($.parseHTML(data)).find('h1 a').attr('href').replace('/', '');
-                let name = $($.parseHTML(data)).find('h1 a').text().trim();
-                resolve({ username, name, email });
-            },
-            error: (xhr, str) => { resolve(-1) }
-        });
-    });
+
+
 }
 
 getAllData();
